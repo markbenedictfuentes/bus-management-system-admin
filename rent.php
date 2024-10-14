4d88c27cd8b647272deaadb0b27579e7a5ea20eb
shang <127384434+markbenedictfuentes@users.noreply.github.com>
2024-10-14 13:29:10 +0800

    Add files via upload

--- rent.php
+++ rent.php
@@ -22,7 +22,7 @@ $name = $_SESSION['name'];
 
     <link rel="stylesheet" href="styles/global.css">
     <link rel="stylesheet" href="styles/login.css">
-    <title>Legal Management</title>
+    <title>Rent Management</title>
 </head>
 
 <body>
