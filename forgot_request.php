00d8e25471e412523284bfc725f82ea5fedf4a3e
yhwach <127384434+markbenedictfuentes@users.noreply.github.com>
2024-10-31 10:09:45 +0800

    Update forgot_request.php

--- forgot_request.php
+++ forgot_request.php
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->isSMTP();
             $mail->Host = 'admin.nexfleetdynamics.com'; // Set the SMTP server to send through
             $mail->SMTPAuth = true;
-            $mail->Username = 'macmac'; // SMTP username
+            $mail->Username = 'macmac@mail.admin.nexfleetdynamics.com'; // SMTP username
             $mail->Password = 'macmac'; // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
